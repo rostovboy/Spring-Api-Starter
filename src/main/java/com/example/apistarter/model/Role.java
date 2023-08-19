34d16d809c0e836f48ff5d055497dcba1ffb3fd4
Alexey T <rostovboy@gmail.com>
2023-08-19 12:07:16 +0300

    Before Migration

---
+++ src/main/java/com/example/apistarter/model/Role.java
@@ -0,0 +1,17 @@
+package com.example.apistarter.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<User> users;
+}
---
+++ src/main/java/com/example/apistarter/model/User.java
@@ -0,0 +1,30 @@
+package com.example.apistarter.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User extends BaseEntity {
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+        inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<Role> roles;
+}
